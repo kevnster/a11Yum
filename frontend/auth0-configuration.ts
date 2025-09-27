632faed05549c1d93c3ed2f6067a58f6a8731d31
kevnster <kevxngyn@gmail.com>
2025-09-27 01:33:53 -0400

    setting up Auth0 with Expo

---
+++ frontend/auth0-configuration.ts
@@ -0,0 +1,8 @@
+import { EXPO_PUBLIC_AUTH0_DOMAIN, EXPO_PUBLIC_AUTH0_CLIENT_ID } from '@env';
+
+const config = {
+  domain: EXPO_PUBLIC_AUTH0_DOMAIN,
+  clientId: EXPO_PUBLIC_AUTH0_CLIENT_ID,
+};
+
+export default config;
---
+++ frontend/types/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const EXPO_PUBLIC_AUTH0_DOMAIN: string;
+  export const EXPO_PUBLIC_AUTH0_CLIENT_ID: string;
+}
