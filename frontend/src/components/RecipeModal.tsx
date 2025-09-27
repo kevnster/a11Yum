aa8e189750d6a23acf9c254595a09a5cd1d8baf6
Tony Wong <t.wong0902@gmail.com>
2025-09-27 12:36:22 -0400

    updated to remove all recipe restriction

--- frontend/src/components/RecipeModal.tsx
+++ frontend/src/components/RecipeModal.tsx
@@ -68,7 +68,7 @@ export const CreateRecipeButton: React.FC<CreateRecipeButtonProps> = ({
             <Animated.View style={[styles.container, { opacity, transform: [{ scale }] }]}>
               <Text style={styles.title}>Create Recipe</Text>
               <Text style={styles.message}>
-                Paste an Allrecipes recipe URL to create a unique recipe screen.
+                Paste a recipe URL to create a unique, curated recipe screen.
               </Text>
 
               <TextInput
